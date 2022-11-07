b2042b96c0110bfae8ec467f31ca28a5a79aeb8f
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-11-07 21:24:08 +0100

    Proposition header

---
+++ client/panier.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Votre Panier</title>
+</head>
+
+<body>
+    <header>
+
+        <p>Design4Green - Équipe 2</p>
+
+        <h1>Panier</h1>
+
+        <div class="container-nav">
+            <a class="icon-text" href="./resume.php">
+                <img src="./assets/images/book.svg" alt="book-icon" class="icon">
+                <p>Résumé</p>
+            </a>
+            <a class="icon-text" href="./panier.php">
+                <img src="./assets/images/cart.svg" alt="cart-icon" class="icon">
+                <p>Panier</p>
+            </a>
+        </div>
+
+    </header>
+</body>
+
+</html>
