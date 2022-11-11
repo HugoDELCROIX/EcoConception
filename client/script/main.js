27a718a7274389ffed2b94b9766eee839ff57e94
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-11-11 10:59:18 +0100

    tri en fonction des lifecycle

--- client/script/main.js
+++ client/script/main.js
@@ -4,32 +4,25 @@ fetch("/client/script/test.json").then(
             data => {
                 if (data.length > 0) {
                     var names = ["N/A", "Maintenance", "Conception", "Administration", "Utilisation", "Réalisation", "Fin de vie", "Acquisition", "Déploiement", "Revalorisation"];
-                    var temp1 = "";
-                    var NA = [];
-                    var temp2 = "";
+                    var temp = "";
 
                     for (let i = 0; i < names.length; i++) {
-                        temp1 += "<tr>";
-                        temp1 += "<th>" + names[i] + "</th>";
-                        temp1 += "<th>Étape clef</th>";
-                        temp1 += "<th>Indicateur</th></tr>";
-                        console.log(names[i].valueOf());
-                        data.forEach((u) => {
-                                temp2 += "<tr>";
-                                temp2 += "<td>" + u.name + "</td>";
-                                temp2 += "<td>" + u.keystep + "</td>";
-                                if (u.indicator == " / ") {
-                                    temp2 += "<td> Pas d'indicateur </td></tr>";
+                        console.log(names[i]);
+                        data.forEach(d => {
+                            if (names[i] == d.lifecycle) {
+                                temp += "<tr>";
+                                temp += "<td>" + d.name + "</td>";
+                                temp += "<td>" + d.keystep + "</td>";
+                                if (d.indicator == " / ") {
+                                    temp += "<td> Pas d'indicateur </td></tr>";
                                 } else {
-                                    temp2 += "<td>" + u.indicator + "</td></tr>";
+                                    temp += "<td>" + d.indicator + "</td></tr>";
                                 }
-                                document.getElementById("data").innerHTML = temp2;
-                            
-                        })
+                                document.getElementById(names[i]).innerHTML = temp;
+                            }
+                        });
                     }
                 };
-                document.getElementById("head").innerHTML = temp1;
-
             }
         )
     }
